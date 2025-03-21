ec165aae88372a6c6210192a7aa57beaf8978ab7
Manreet Thind <Mthind2315@conestogac.on.ca>
2025-03-20 20:46:40 -0400

    Added header file for Client

---
+++ chat-client/inc/client.h
@@ -0,0 +1,31 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <time.h>
+#include <ncurses.h>
+#include <ifaddrs.h>
+
+#define BUFFER_SIZE 1024
+#define MESSAGE_CHUNK_SIZE 40
+
+// Function prototypes
+void init_ui(void);
+void add_to_history(char *message);
+void display_message(char *buffer);
+void *handle_incoming_messages(void *arg);
+void get_timestamp(char *timestamp);
+void get_my_ip(int socket);
+
+// Global variables
+extern WINDOW *input_win;
+extern WINDOW *output_win;
+
+#endif
