055a35e9505fe25306d0336b44c633d0d7bcb87c
Manreet Thind <Mthind2315@conestogac.on.ca>
2025-03-21 21:22:47 -0400

    Added get_timestamp function in Server-side

--- chat-server/src/utils.c
+++ chat-server/src/utils.c
@@ -55,3 +55,10 @@ void split_message(char *message, char chunks[][MESSAGE_CHUNK_SIZE + 1], int *nu
         *num_chunks = 1;
     }
 }
+
+// Get current timestamp in HH:MM:SS format
+void get_timestamp(char *timestamp) {
+    time_t now = time(NULL);
+    struct tm *tm = localtime(&now);
+    sprintf(timestamp, "%02d:%02d:%02d", tm->tm_hour, tm->tm_min, tm->tm_sec);
+}
